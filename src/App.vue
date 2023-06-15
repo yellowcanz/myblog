ae263bd4a03a8850c48014914eaa5cac5561890a
yellowcanz <75051323+yellowcanz@users.noreply.github.com>
2023-06-15 18:22:25 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import NavBar from '@/views/componentes/NavBar.vue'
-import FooterPage from '@/views/componentes/FooterPage.vue'
+import NavBar from './views/componentes/NavBar.vue'
+import FooterPage from './views/componentes/FooterPage.vue'
 
 </script>
 
