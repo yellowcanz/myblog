256b5c19f41dcfa352f1138299511436c946099b
yellowcanz <75051323+yellowcanz@users.noreply.github.com>
2023-06-15 15:48:47 +0000

    tiaoshi

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import code from '@yankeeinlondon/code-builder'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'./',
+  base:'/',
   plugins: [
     Vue({
       include: [/\.vue$/, /\.md$/], // <--
