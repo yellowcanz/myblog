7a92f1cda95e9e79ed0495b4c8fd18ff0770ae2b
yellowcanz <75051323+yellowcanz@users.noreply.github.com>
2023-06-15 16:51:12 +0000

    测试

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,16 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Markdown from 'vite-plugin-md'
-import code from '@yankeeinlondon/code-builder'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:'/',
+  base:'/',
   plugins: [
     Vue({
       include: [/\.vue$/, /\.md$/], // <--
     }),
-    Markdown({
-      builders: [code()]
-    }),
+    Markdown(),
   ],
   resolve: {
     alias: {
