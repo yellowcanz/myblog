6d6635905cb3fa47457b8e779f8d0a82f4b9e275
“Yellowcanz” <1609693846@qq.com>
2023-06-25 21:20:19 +0800

    修改错误样式

--- src/components/MdFilePage.vue
+++ src/components/MdFilePage.vue
@@ -63,7 +63,7 @@ onMounted(async () => {
 :deep(.markdown-container h5) {
   @apply text-base font-bold mt-4 mb-2;
 }
-:deep(.markdown-container h3) {
+:deep(.markdown-container h6) {
   @apply text-sm font-bold mt-4 mb-2;
 }
 
